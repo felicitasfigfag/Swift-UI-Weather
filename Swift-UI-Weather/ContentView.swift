0facf395190615b693bb237c177c568977ca0592
Felicitas <felifigueroaf@gmail.com>
2022-11-25 17:57:10 +0100

    Initial UI

---
+++ Swift-UI-Weather/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Swift-UI-Weather
+//
+//  Created by Felicitas Figueroa Fagalde on 25.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-UI-Weather/Swift_UI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_WeatherApp.swift
+//  Swift-UI-Weather
+//
+//  Created by Felicitas Figueroa Fagalde on 25.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
