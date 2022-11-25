5b605104bd45c9cbbe7cb7729a55bce56870ccd1
Felicitas <felifigueroaf@gmail.com>
2022-11-25 19:28:05 +0100

    Finished UI

--- Swift-UI-Weather/ContentView.swift
+++ Swift-UI-Weather/ContentView.swift
@@ -19,14 +19,73 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack(spacing: 8){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("66°")
+                        .font(.system(size: 70, weight: .medium, design: .default))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                
+                HStack(spacing: 20){
+                    WeatherDayView(dayOfWeek: "Tue", imageName: "sun.max.fill", temperature: 66)
+                    WeatherDayView(dayOfWeek: "Wed", imageName: "wind", temperature: 45)
+                    WeatherDayView(dayOfWeek: "Thu", imageName: "sunset.fill", temperature: 68)
+                    WeatherDayView(dayOfWeek: "Fri", imageName: "moon.stars.fill", temperature: 76)
+                    WeatherDayView(dayOfWeek: "Fri", imageName: "moon.stars.fill", temperature: 76)
+                    WeatherDayView(dayOfWeek: "Sat", imageName: "cloud.sun.fill", temperature: 76)
+                }
+                
                 Spacer()
             }
         }
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 5){
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40 , height: 40)
+            Text("\(temperature)")
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
     }
 }
